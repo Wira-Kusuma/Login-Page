314531accb19b36fda6c5b17620cb7b9cc8d9d68
Wira Kusuma Phandawa <wirakusuma5566@gmail.com>
2025-06-16 07:30:31 +0700

    add js swich btn

--- script.js
+++ script.js
@@ -10,18 +10,24 @@ let registerBtn = document.getElementById("registerBtn");
 let loginBtn = document.getElementById("loginBtn");
 
 
-toLogin.addEventListener("click", function() {
+toLogin.addEventListener("click", loginSwich);
+toRegister.addEventListener("click", registerSwich);
+
+loginBtn.addEventListener("click", loginSwich);
+registerBtn.addEventListener("click", registerSwich);
+
+function loginSwich() {
     registerForm.style.display="none";
     loginForm.style.display="flex";
 
     loginBtn.style.display="none";
     registerBtn.style.display="flex";
-});
+}
 
-toRegister.addEventListener("click", function() {
+function registerSwich() {
     loginForm.style.display="none";
     registerForm.style.display="flex";
 
     registerBtn.style.display="none";
     loginBtn.style.display="flex";
-});
+}
